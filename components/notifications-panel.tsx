e9f8c0c999bd45d233f2f834a8b3e4dbaf362e4e
Dqniel <166188737+LDanielOchoa@users.noreply.github.com>
2024-12-27 15:22:24 -0500

    Update notifications-panel.tsx

--- components/notifications-panel.tsx
+++ components/notifications-panel.tsx
@@ -94,7 +94,7 @@ export default function NotificationsPanel({ onClose, onMarkAllAsRead }: Notific
     try {
       const newStatus = currentStatus === 0 ? 1 : 2
   
-      const response = await fetch(`http:s//solicitud-permisos.onrender.com/requests/${notificationId}/notifications`, {
+      const response = await fetch(`https://solicitud-permisos.onrender.com/requests/${notificationId}/notifications`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ notification_status: newStatus }),
