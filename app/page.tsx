fa2d9b4ae2abbb8fd7a87ff047ab639c5e870275
LDanielOchoa <daniel.ochoaa.cass@gmail.com>
2025-05-22 12:42:43 -0500

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 'use client'
 
 import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 export default function Page() {
   const router = useRouter()
 
   useEffect(() => {
-    router.push('https://solicitud-permisos.sao6.com.co/')
-  }, [])
+    window.location.href = 'https://solicitud-permisos.sao6.com.co/'
+  }, [router])
 
-  return null
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <p>Redirigiendo...</p>
+    </div>
+  )
 }
